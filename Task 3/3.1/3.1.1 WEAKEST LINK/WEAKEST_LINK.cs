6dcde744607a239c6eaaa365eab89687be518aaf
Владимир Худошин <vladimirhudosin91@gmail.com>
2022-02-10 20:19:35 +0400

    Task 3.1 done

---
+++ Task 3/3.1/3.1.1 WEAKEST LINK/WEAKEST_LINK.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WEAKEST_LINK
+{
+    public class WEAKEST_LINK
+    {
+        class Person
+        {
+            public int Id { get; set; }
+            public override string ToString()
+            {
+                return Id.ToString();
+            }
+        }
+        static void Main(string[] args)
+        {
+            List<Person> persons = new List<Person>();
+            Console.WriteLine("Введите N");
+            int n = Check(Console.ReadLine());
+            Console.WriteLine("Введите, какой по счету человек будет вычеркнут каждый раунд:");
+            int dropOutNumber = Check(Console.ReadLine());
+            int round = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                persons.Add(new Person { Id = i });
+            }
+            
+            while (true)
+            {
+                if (persons.Count < dropOutNumber)
+                {
+                    Console.WriteLine("Игра окончена. Невозможно вычеркнуть больше людей.");
+                    break;
+                }
+                else 
+                {
+                    persons.RemoveAt(dropOutNumber - 1);
+                    Console.WriteLine("Раунд {0}. Вычеркнут человек. Людей осталось: {1}", round++, persons.Count);
+                }
+            }
+        }
+        public static int Check(string str)
+        {
+            if (Int32.TryParse(str, out int result))
+            {
+                return result;
+            }
+            else
+            {
+                Console.WriteLine("Некорректное значение, повторите попытку");
+                return Check(Console.ReadLine());
+            }
+        }
+    }
+}
---
+++ Task 3/3.1/3.1.2 TEXT ANALYSIS/TEXT ANALYSIS.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TEXT_ANALYSIS
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите текст");
+            string text = Console.ReadLine();
+            char[] separators = new char[] { ' ', ',', '.', '!', '?', ':', ';', '\'', '"' };
+            string[] words = text.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+            Dictionary<string,int> repeatedWords = new Dictionary<string,int>();
+            foreach (string word in words)
+            {
+                if (CorrectWord(word))
+                {
+                    string wordToLower = word.ToLower();
+                    if (repeatedWords.ContainsKey(wordToLower))
+                    {
+                        repeatedWords[wordToLower]++;
+                    }
+                    else
+                    {
+                        repeatedWords.Add(wordToLower, 1);
+                    }
+                }               
+            }
+            foreach (var word in repeatedWords)
+            {
+                Console.WriteLine("Word: {0,20} \tcount: {1}", word.Key, word.Value);
+            }
+            FavoriteWords(repeatedWords);
+        }
+        public static bool CorrectWord(string word)
+        {
+            if (word[0] == '-') return false;
+            foreach (char c in word)
+            {
+                if (char.IsNumber(c)) return false;
+            }
+            return true;
+        }
+
+        public static void FavoriteWords(Dictionary<string, int> words)
+        {
+            var res = words.Where(x => x.Value > 5);
+            if(res.Count() > 0)
+            {
+                Console.WriteLine("Favorite words");
+                foreach (var word in res)
+                {
+                    Console.WriteLine(word.Key);
+                }
+            }
+            else Console.WriteLine("No favorite words");
+        }
+    }
+    
+}
